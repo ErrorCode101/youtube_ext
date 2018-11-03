ab36032632d9afbf151fc231f4f86309b205709b
MusthaqAa <musthaqaadhil@gmail.com>
2018-11-03 22:43:53 +0530

    Default Changelist

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,17 +1,20 @@
 import {Component} from '@angular/core';
 
+declare var YT: any;
+
 @Component({
     selector: 'app-player',
     templateUrl: './player.component.html',
     styleUrls: ['./player.component.css']
 })
-export class PlayerComponent {
+export class PlayerComponent{
 
     player: YT.Player;
     public id: string = 'qDuKsiwS5xw';
 
-    constructor() {
+    constructor(){
     }
+
 
     public savePlayer(player) {
         this.player = player;
@@ -22,4 +25,12 @@ export class PlayerComponent {
         console.log('player state', event.data);
     }
 
+    private loadPlayer() {
+        let tag = document.createElement('script');
+        tag.src = 'https://www.youtube.com/iframe_api';
+
+        let firstScriptTag = document.getElementById('player')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    }
 }
